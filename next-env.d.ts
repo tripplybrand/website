3b530f401df14759ead5cd6b21e5c945f8e5087a
tripplybrand <tripplybrand@gmail.com>
2021-05-12 17:45:17 -0400

    Add declare module svg in next-env.d.ts to solve svg import issue.

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,8 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+declare module '*.svg' {
+  import * as React from 'react'
+  const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>
+  export default ReactComponent
+}
