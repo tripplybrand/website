f8e645d28b496e41640f48b9a0724b3ce6707d1e
tripplybrand <tripplybrand@gmail.com>
2021-09-21 12:10:25 -0400

    Make focus ring thinner below small screen size.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -73,7 +73,7 @@ const SectionLink = ({ text, href }: SectionLinkProps) => {
       <a
         className="text-lg sm:text-2xl md:text-4xl font-light tracking-wider underline 
       text-black hover:text-matisse-red-200
-      focus:outline-none focus:ring-2 focus:ring-matisse-red-200 focus:ring-offset-2 focus:ring-opacity-60"
+      focus:outline-none focus:ring-1 sm:focus:ring-2 focus:ring-matisse-red-200 focus:ring-offset-2 focus:ring-opacity-60"
       >
         {text}
       </a>
